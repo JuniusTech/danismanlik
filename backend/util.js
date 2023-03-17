ce99890f8d1d8b07cb9d0dd7e2b67ba9efb8db3d
mustafa mehel <mehelx5207@gmail.com>
2023-03-17 15:42:56 +0300

    e

--- backend/util.js
+++ backend/util.js
@@ -31,10 +31,8 @@ const isAuth = (req, res, next) => {
   }
 };
 const isLawyer = (req, res, next) => {
-  // const authorization = req.headers.authorization;
   const token = req.cookies.jwt;
   if (token) {
-    // const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         res.status(401).send({ message: "Invalid Token" });
