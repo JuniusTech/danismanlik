0d9711228fed34c2af872a6af1f562ff6dd01206
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-04-25 15:06:24 +0300

    loginPages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/style.css";
 import SearchButton from "./Search";
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.svg";
+import AvukatLoginPage from "../pages/AvukatLoginPage";
+import MuvekkilLoginPage from "../pages/MuvekkilLoginPage";
+import MuvekkilSignPage from "../pages/MuvekkilSignPage";
+import AvukatSignPage from "../pages/AvukatSignPage";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
+
   return (
     <>
       <nav className="navbar navbar-expand-lg">
@@ -18,12 +32,14 @@ const Navbar = () => {
               Avukat misiniz?
             </button>
             <button
+              onClick={() => setShow(true)}
               className="ms-2 btn btn-outline-black"
               style={{ border: "1px solid black" }}
             >
               KAYIT OL
             </button>
             <button
+              onClick={() => setShow2(true)}
               className="ms-2 btn btn-outline-black"
               style={{ border: "1px solid black" }}
             >
@@ -31,6 +47,16 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+        <MuvekkilLoginPage
+          show={show}
+          handleClose={handleClose}
+          onHide={() => setShow(false)}
+        />
+        <MuvekkilSignPage
+          show={show2}
+          handleClose={handleClose2}
+          onHide={() => setShow2(false)}
+        />
       </nav>
     </>
   );
