d4415cee775e9761b8344d55199ce38dac6e58a5
SadiKISLAR <sadi22kislar@gmail.com>
2023-04-28 16:05:59 +0300

    main search page direct

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+  const [branchs, setBranchs] = useState([])
+  const [branch, setBranch] = useState("")
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("https://danis.onrender.com/api/branchs")
+      .then((response) => {
+        setBranchs(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [branch]);
+
   return (
     <div>
       <form className='search' >
-        <input
-          type="search"
-          className='home-search-input'
-          placeholder='Ornek: Bosanmak istiyorum...' />
-        <button className='home-button text-white' type='submit'>Avukat Ara</button>
+
+        <select className='search-select' value={branch} name="branch"
+          onChange={(e) => setBranch(e.target.value)
+          } title="Branş Seç" id="navbarScrollingDropdown">
+          <option defaultValue="all">Branş Seç</option>
+          {branchs
+            ?.sort((a, b) => a.title.localeCompare(b.title))
+            .map((item) => (
+              <option key={item._id} value={item.title}>
+                {item.title}
+              </option>
+            ))}
+        </select>
+        <button className='home-button text-white' type='submit' onClick={() => navigate(`/search?branch=${branch}&query=all&price=all&rating=all&order=newest&page=1`)}>Avukat Ara</button>
       </form>
     </div>
   )
