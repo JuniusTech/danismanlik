7af636ec761423f8ef720fac4b0e9140c54b485d
mustafa mehel <mehelx5207@gmail.com>
2023-05-18 14:20:12 +0300

    footer style and link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import "../css/Footer.css";
 import {
   MDBFooter,
   MDBContainer,
@@ -6,153 +7,240 @@ import {
   MDBInput,
   MDBCol,
   MDBRow,
-  MDBBtn
-} from 'mdb-react-ui-kit';
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <MDBFooter className='text-center' color='white' bgColor='dark'>
-      <MDBContainer className='p-4'>    
-
-        <section className=''>
+    <MDBFooter className="text-center footer">
+      <MDBContainer className="p-4">
+        <section className="">
           <MDBRow>
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h3 className='text'>Popüler Aramalar</h3>
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+              <h3 className="text">Popüler Aramalar</h3>
 
-              <ul className='list-unstyled mb-0' >
+              <ul className="list-unstyled mb-0">
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }} >
+                  <Link
+                    to="/search?branch=all&query=all&price=all&rating=all&order=newest&page=1"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Bosanma Avukati
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="/search?branch=all&query=all&price=all&rating=all&order=newest&page=1"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Icra Avukati
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="/search?branch=all&query=all&price=all&rating=all&order=newest&page=1"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Ceza Avukati
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="/search?branch=all&query=all&price=all&rating=all&order=newest&page=1"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Vergi Avukati
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="/search?branch=all&query=all&price=all&rating=all&order=newest&page=1"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Sigorta Avukati
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </MDBCol>
 
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0' style={{ textDecoration: 'none' }}>
-              <h3 className='text'>Müvekkiller Icin</h3>
+            <MDBCol
+              lg="3"
+              md="6"
+              className="mb-4 mb-md-0"
+              style={{ textDecoration: "none" }}
+            >
+              <h3 className="text">Müvekkiller Icin</h3>
 
-              <ul className='list-unstyled mb-0'>
+              <ul className="list-unstyled mb-0">
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="/search?branch=all&query=all&price=all&rating=all&order=newest&page=1"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Avukatlar
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to=""
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Sikca Sorulan Sorular
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Veri Guvenligi
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Müvekkil Uyelik Sozlesmesi
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Müvekkil Aydinlatma Metni
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </MDBCol>
 
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h3 className='text'>Avukatlar Icin</h3>
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+              <h3 className="text">Avukatlar Icin</h3>
 
-              <ul className='list-unstyled mb-0' style={{ textDecoration: 'none' }}>
+              <ul
+                className="list-unstyled mb-0"
+                style={{ textDecoration: "none" }}
+              >
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     One Cikan Ozellikler
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Fiyatlar
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Veri Guvenligi
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Avukat Uyelik Sozlesmesi
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Avukat Aydinlatma Metni
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </MDBCol>
 
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h3 className='text'>Hakkimizda</h3>
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+              <h3 className="text">Hakkimizda</h3>
 
-              <ul className='list-unstyled mb-0'>
+              <ul className="list-unstyled mb-0">
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Biz Kimiz?
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Iletisim
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#!' className='text-white' style={{ textDecoration: 'none' }}>
+                  <Link
+                    to="#!"
+                    className="text-white opacity-50"
+                    style={{ textDecoration: "none" }}
+                  >
                     Cerezlere iliskin Aydinlatma Metni
-                  </a>
+                  </Link>
                 </li>
-                
               </ul>
             </MDBCol>
           </MDBRow>
         </section>
 
-        <section className='mb-10'>
-          <p><br></br>
+        <section className="mb-10 opacity-50">
+          <p>
+            <br></br>
             www.avukatimol.com- Avukat bul ve randevu al
           </p>
         </section>
 
-        <section className='mb-10'>
+        <section className="mb-10 opacity-50">
           <p>
-            Is bu sayfada yer alan yorummlar, ilgili avukatin dogrudan veya dolayli emri, talebi ve/veya ricasi olmaksizin, ilgili müvekkil tarafindan bagimsiz olarak yazilmaktadir. Bu web sitesinin temel amaci hukuk alaninda kamuoyunun daha iyi bilgilendirilmesini saglamaktir. avukatimol.com bir basvru hizmeti degildir ve herhangi bir Hukuk Hizmeti Saglayicisini tvsiye etmemektedir.
+            Is bu sayfada yer alan yorummlar, ilgili avukatin dogrudan veya
+            dolayli emri, talebi ve/veya ricasi olmaksizin, ilgili müvekkil
+            tarafindan bagimsiz olarak yazilmaktadir. Bu web sitesinin temel
+            amaci hukuk alaninda kamuoyunun daha iyi bilgilendirilmesini
+            saglamaktir. avukatimol.com bir basvru hizmeti degildir ve herhangi
+            bir Hukuk Hizmeti Saglayicisini tavsiye etmemektedir.
           </p>
         </section>
       </MDBContainer>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div
+        className="text-center p-3 opacity-50"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
         © 2023 JuniusTech Teknoloji A.S. Tüm haklari saklidir.
-        
       </div>
     </MDBFooter>
   );
