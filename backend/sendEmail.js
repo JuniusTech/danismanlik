895f61ce708aab1ba363a9e8370c1fc31a679ab6
SadiKISLAR <sadi22kislar@gmail.com>
2023-06-17 16:55:22 +0300

    send email change

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -6,16 +6,16 @@ module.exports = async (userEmail, subject, htmlTemplate) => {
       host: "sandbox.smtp.mailtrap.io",
       port: 2525,
       auth: {
-        // user: process.env.EMAIL,
-        // pass: process.env.PASSWORD,
-        user: "df941bc2eb0b92",
-        pass: "6643ef199994ba",
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+        // user: "df941bc2eb0b92",
+        // pass: "6643ef199994ba",
       },
     });
 
     const mailOptions = {
-      // from: process.env.EMAIL,
-      from: "df941bc2eb0b92",
+      from: process.env.EMAIL,
+      // from: "df941bc2eb0b92",
       to: userEmail,
       subject: subject,
       html: htmlTemplate,
