b011bdd891a3249ff465fd891222e9ded1fdcc4b
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-06-21 17:42:46 +0300

    kredi kartı sayfası

--- frontend/src/AppRoute.js
+++ frontend/src/AppRoute.js
@@ -15,7 +15,7 @@ import HesapDoğrulamaEkranı from "./components/şifre yenileme/HesapDoğrulama
 import LawyerDashboard from "./pages/LawyerDashboard";
 import LawyerHomePage from "./pages/LawyerHomePage";
 import SSSMain from "./components/sıkSorulanSorular/SSSMain.jsx";
-import FiyatlandırmaSayfası from "./pages/FiyatlandırmaSayfası";
+import Ucretlendirme from "./components/Ucretlendirme/UcretlendirmeMain.jsx";
 import SifreSıfırlama from "./pages/SifreSıfırlama";
 import YeniSifreBelirle from "./components/şifre yenileme/YeniSifreBelirle";
 
@@ -36,7 +36,7 @@ const AppRoute = () => {
       <Route path="/lawyerDashboard" element={<LawyerDashboard />} />
       <Route path="/lawyerhomepage" element={<LawyerHomePage />} />
       <Route path="/sıksorulansorular" element={<SSSMain />} />
-      <Route path="/fiyatlandırma" element={<FiyatlandırmaSayfası />} />
+      <Route path="/ücretlendirme" element={<Ucretlendirme />} />
 
       <Route
         path="/reset-password-law/:id/:token"
